901b508586f12182f12f74ae160dd5bcc21a0114
VitorHugoAntunes <vitor.passos3@etec.sp.gov.br>
2021-02-24 20:51:36 -0300

    Challenge interface, function for random challenge

--- src/contexts/ChallengesContext.tsx
+++ src/contexts/ChallengesContext.tsx
@@ -1,9 +1,16 @@
 import { createContext, useState, ReactNode } from 'react';
+import challenges from '../../challenges.json';
 
+interface Challenge {
+  type: 'body' | 'eye';
+  description: string;
+  amount: number;
+}
 interface ChallengesContextData {
   level: number;
   currentExperience: number;
   challengesCompleted: number;
+  activeChallenge: Challenge;
   levelUp: () => void;
   startNewChallenge: () => void;
 }
@@ -19,22 +26,28 @@ export function ChallengesProvider({ children }: ChallengesProviderProps) {
   const [currentExperience, setCurrentExperience] = useState(0);
   const [challengesCompleted, setChallengesCompleted] = useState(0);
 
+  const [activeChallenge, setActiveChallenge] = useState(null);
+
   function levelUp() {
     setLevel(level + 1);
   }
 
   function startNewChallenge() {
-    console.log('New challenge');
+    const ramdomChallengeIndex = Math.floor(Math.random() * challenges.length);
+    const challenge = challenges[ramdomChallengeIndex];
+
+    setActiveChallenge(challenge);
   }
 
   return (
     <ChallengesContext.Provider
       value={{
-        level: 1,
+        level,
         currentExperience,
         challengesCompleted,
         levelUp,
         startNewChallenge,
+        activeChallenge,
       }}
     >
       {children}
